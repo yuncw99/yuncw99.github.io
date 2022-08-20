d2dab6bf46f9988b7701eba18840849899b90e56
yuncw991025 <y99275739@gmail.com>
2022-08-20 15:12:56 +0900

    test react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {
+    setCounter((current) => current + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Chanwoo Yun {counter}</h3>
+      <button onClick={onClick}>Click Me!</button>
     </div>
   );
 }
